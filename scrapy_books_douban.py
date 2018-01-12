4a844fd19a4cde95daa1ab592a7d7e133903ae1b
flying-fisher <cuijunlei2016@gmail.com>
2018-01-12 21:06:17 +0800

    爬取豆瓣读书上的计算机书籍信息

---
+++ scrapy_books_douban.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#豆瓣励志书籍
+
+import requests
+import re
+import time
+
+contents = requests.get('https://book.douban.com/latest?icn=index-latestbook-all').text
+pattern = re.compile('<li.*?detail-frame.*?href=.*?>(.*?)</a>.*?color-gray">(.*?)</p>',re.S)
+
+resulta = re.findall(pattern,contents)
+print(resulta)
+for resultas in resulta:
+    name,year = resultas
+    year = re.sub('\s','',year)
+    print(name,year)
+    time.sleep(1)
